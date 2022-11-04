dc28666e73e6592eb0270c79f518637ec72b166f
Connor Harris <connorharris@Connors-MacBook-Pro-2.local>
2022-11-04 11:16:56 -0400

    added Data to Json

--- models/SpaceItem.js
+++ models/SpaceItem.js
@@ -3,15 +3,40 @@ const Schema = mongoose.Schema;
 
 let SpaceItem = new Schema({
   name: { type: String, required: true },
-  type: { type: String, required: true },
-  size: { type: String, required: true },
+  type: {
+    type: String,
+    required: true,
+    enum: [
+      "Continental",
+      "Ocean",
+      "Barren",
+      "Tropical",
+      "Desert",
+      "Arid",
+      "Arctic",
+      "Alpine",
+      "Star",
+      "Station",
+      "Ship",
+      "Gas planet",
+      "Astroid",
+      "Specialty",
+      "Colony",
+      "Moon"
+    ],
+  },
+  size: {
+    type: String,
+    required: true,
+    enum: ["Tiny", "Small", "Medium", "Large", "Giant"],
+  },
   price: { type: Number, required: true },
   location: { type: String, required: true },
   image: [{ type: Schema.Types.ObjectId, ref: "images" }],
-  owner: { type: Schema.Types.ObjectId, ref: 'userDatas' },
+  owner: { type: Schema.Types.ObjectId, ref: "userDatas" },
   rating: { type: Number },
-  isBrought: { type: Boolean },
-  isHab: { type: Boolean }
+  isBought: { type: Boolean },
+  isHab: { type: Boolean },
 });
 
 export default mongoose.model("spaceItems", SpaceItem);
