525c3a7c65939967a869fe5d0d93dbf622c6f955
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-11-04 14:10:57 -0400

    update argon for testing

--- helpers/restrictArgon.js
+++ helpers/restrictArgon.js
@@ -2,7 +2,7 @@ import paseto from 'paseto'
 const { V4 } = paseto
 import { createPublicKey } from 'crypto'
 
-const key = createPrivateKey(publicKey)
+const key = createPublicKey(publicKey)
 const token = 'v4.public.eyJ1cm46ZXhhbXBsZTpjbGFpbSI6ImZvbyIsImlhdCI6IjIwMjEtMDctMTlUMTA6MTM6MjIuOTM3WiIsImV4cCI6IjIwMjEtMDctMTlUMTI6MTM6MjIuOTM3WiIsImF1ZCI6InVybjpleGFtcGxlOmNsaWVudCIsImlzcyI6Imh0dHBzOi8vb3AuZXhhbXBsZS5jb20ifYZrfK1eH8d7Scp218_DPEX8H3ElIfzWWMu9UQVZYjyV585BEBV0wTRk-vZgtXq0y5z0euOE48a2Yd6TLKfA5Qs'
 
 const restrict = (req, res, next) => {
