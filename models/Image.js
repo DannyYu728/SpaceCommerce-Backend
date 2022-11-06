ef4d103642402ba3ff545604394766a8aad12d3d
Danny Yu <tiramisu@Dannys-MBP.lan>
2022-11-05 22:02:41 -0400

    remove unused schema type

--- models/Image.js
+++ models/Image.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 let Image = new Schema({
-  source: { type: Schema.Types.ObjectId, ref: 'userDatas' },
-  spaceItem: { type: Schema.Types.ObjectId, ref: 'spaceItems'},
+  name: { type: String },
   image: { type: String },
 });
 
